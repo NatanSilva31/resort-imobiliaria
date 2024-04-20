c7b3157de33e35da2cb361f88e2df9cb74a0c309
GuilhermeFL1 <111026068+GuilhermeFL1@users.noreply.github.com>
2024-04-20 20:25:45 -0300

    Adicionando cores e funcionalidades
    Adicionei uma funcionalidade na tela de agendamento que não deixa o usuário selecionar datas retroativas.

--- resort_imobiliaria/imobiliaria/src/main/webapp/FormularioVenda/venda.js
+++ resort_imobiliaria/imobiliaria/src/main/webapp/FormularioVenda/venda.js
@@ -2,15 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("booking-form");
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Previne o envio real do formulário para demonstração
+        event.preventDefault(); // Impede o envio real do formulário para fins de demonstração
 
         const name = document.getElementById("name").value.trim();
         const email = document.getElementById("email").value.trim();
         const date = document.getElementById("date").value.trim();
         const property = document.getElementById("property").value.trim();
 
+        // Obtém a data atual
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0); // Define a hora para meia-noite
+
+        // Analisa a data selecionada do campo de entrada
+        const selectedDate = new Date(date);
+
         if (!name || !email || !date || property === "") {
             alert("Por favor, preencha todos os campos.");
+        } else if (selectedDate < currentDate) {
+            alert("A data selecionada não pode ser retroativa. Por favor, escolha uma data futura.");
         } else {
             // Exibe a notificação
             const notification = document.getElementById("notification");
